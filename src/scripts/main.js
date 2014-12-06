90b1b27c24482c62c2c9ab4710b6b216c123d6ae
james2doyle <james2doyle@gmail.com>
2014-12-06 13:52:31 -0500

    update javascript

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 var firstChoice = '0';
 var secondChoice = '0';
 var talents = {};
+var callbackList = [];
+var $audio;
 
 function shuffle(sourceArray) {
   for (var n = 0; n < sourceArray.length - 1; n++) {
@@ -12,19 +14,42 @@ function shuffle(sourceArray) {
   return sourceArray;
 }
 
+function updateSidebar() {
+  var i = callbackList.length - 1;
+  var template = "<div class=\"clearfix saved-talent\"><div class=\"col col-2\"><img src=\""+callbackList[i].original_image+"\" alt=\"\"></div><div class=\"col col-9\"><div class=\"px2\"><h3 class=\"mt0\">"+callbackList[i].name+"</h3><a href=\""+callbackList[i].url+"\"><i class=\"fa fa-thumbs-o-up\"></i> Hire</a><a href=\""+callbackList[i].url+"\"><i class=\"ml1 fa fa-dollar\"></i> Request Quote</a></div></div></div>";
+  $('#sidebar').append(template);
+}
+
 function resetBoard() {
   firstChoice = '0';
   secondChoice = '0';
   $('.talents').addClass('disabled');
 }
 
+function addToList() {
+  if (confirm('Add This Voice To You Callback List?')) {
+    var id = $('[data-id='+secondChoice+']')[0].id;
+    callbackList.push(talents[id[1]]);
+    updateSidebar();
+  } else {
+    // they dont want this person
+  }
+}
+
+function stopAudio() {
+  $audio[0].pause();
+  $audio.removeAttr('src');
+}
+
 function handleWin() {
-  console.log('They Match!');
+  alert('They Match!');
+  stopAudio();
+  addToList();
+  resetBoard();
 }
 
 function handleLoss() {
-  console.error('No Match!');
-  resetBoard();
+  alert('No Match!');
 }
 
 function checkChoice() {
@@ -44,6 +69,9 @@ function templateData(data, callback) {
       'src': data[i].image,
       'alt': data[i].name
     });
+  }
+  data = shuffle(data);
+  for (var i = data.length - 1; i >= 0; i--) {
     $('#v'+i).attr({
       'data-id': data[i].id,
       'data-audio': data[i].audio,
@@ -54,17 +82,21 @@ function templateData(data, callback) {
 }
 
 function init() {
+  console.log('game start');
   var $voices = $('.voices');
   var $talents = $('.talents');
+  $audio = $('audio');
   $voices.on('click', '.col', function() {
-    var $target = $(this).find('img').attr('data-id');
-    firstChoice = $target;
-    console.log($target);
+    var $target = $(this).find('img');
+    firstChoice = $target.attr('data-id');
+    console.log($target.attr('data-id'));
+    $audio.attr('src', $target.attr('data-audio'));
+    $audio[0].play();
     $talents.removeClass('disabled');
   });
   $talents.on('click', '.col', function() {
-    var $target = $(this).find('img').attr('data-id');
-    secondChoice = $target;
+    var $target = $(this).find('img');
+    secondChoice = $target.attr('data-id');
     console.log($target);
     checkChoice();
   });
@@ -73,7 +105,7 @@ function init() {
 jQuery(function() {
   $.get('cache.json').then(function(res) {
     talents = res;
-    console.log(talents);
+    // console.dir(talents);
     templateData(talents, function() {
       $(document.body).removeClass('loading');
       init();
